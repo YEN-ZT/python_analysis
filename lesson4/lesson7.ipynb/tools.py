ff6ef209cd1bc31737de40f29f18540da2b406fa
YEN-ZT <s429216@gmail.com>
2025-05-04 03:53:44 +0000

    lesson7-1.py

---
+++ lesson4/lesson7.ipynb/tools.py
@@ -0,0 +1,14 @@
+import tools
+def main():
+    height:int = int(input("請輸入身高公分"))
+    weight:int = int(input("請輸入體重公斤"))
+    BMI:float = tools.get(weught,heught)
+
+    print(f"身高:{height}""cm")
+    print(f"體重:{weight}""kg")
+    print(f"BMI:{BMI}")
+    status:str = tools.get_status(BMI)
+    print(f"狀態為:{status}")
+
+    if __name__ == '__main__':
+    main()
