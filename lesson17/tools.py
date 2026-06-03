bf91a3ed35d1be0195dfbb1129631ebf4087749c
yen <s429216@gmail.com>
2026-06-03 18:03:34 +0800

    tools.py

---
+++ lesson17/tools.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+PI:float = 3.1415926
+
+def func1(a:int, b:int) -> int:
+    return a + b
+
+@dataclass
+class Student:
+    name:str
+    chinese:int
+    math:int
+    english:int
